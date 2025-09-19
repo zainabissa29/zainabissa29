ab1395e252fa09ccb25080e10246303bcf4d2e45
Zainab Issa <zainab.issa29@yahoo.com>
2025-09-18 22:31:47 -0600

    Create WN10-SO-000185.ps1

---
+++ STIGS/WN10-SO-000185.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script sets the AllowOnlineID registry value to 0 under PKU2U,
+    disabling online ID authentication to enhance local security.
+
+.NOTES
+    Author          : Zainab Issa
+    LinkedIn        : linkedin.com/in/zainabissa-cybersecurity/
+    GitHub          : github.com/zainabissa29
+    Date Created    : 2025-09-18
+    Last Modified   : 2025-09-18
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000185
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+
+1. Open PowerShell **as Administrator**.
+2. Run the script below:
+#>
+
+```powershell
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Control\LSA\pku2u"
+$valueName = "AllowOnlineID"
+$valueData = 0
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the AllowOnlineID value (REG_DWORD)
+New-ItemProperty -Path $regPath -Name $valueName -PropertyType DWord -Value $valueData -Force
+
+Write-Output "Registry value '$valueName' set to $valueData under $regPath"
