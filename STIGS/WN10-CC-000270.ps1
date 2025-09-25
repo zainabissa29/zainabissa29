0385949147028d9d4df79c925957cc36a50d06cf
Zainab Issa <zainab.issa29@yahoo.com>
2025-09-25 11:29:58 -0600

    Create  WN10-CC-000270.ps1

---
+++ STIGS/WN10-CC-000270.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    This PowerShell script sets the DisablePasswordSaving registry value to 1 under the Terminal Services policy key,
+    disabling the ability to save Remote Desktop credentials as required by Windows 10 STIG.
+
+.NOTES
+    Author          : Zainab Issa
+    LinkedIn        : linkedin.com/in/zainabissa-cybersecurity/
+    GitHub          : github.com/zainabissa29
+    Date Created    : 2025-09-25
+    Last Modified   : 2025-09-25
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000270
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : Windows 10
+    PowerShell Ver. : 5.1
+
+#>
+
+```powershell
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+$valueName = "DisablePasswordSaving"
+$valueData = 1
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the DisablePasswordSaving value (REG_DWORD)
+New-ItemProperty -Path $regPath -Name $valueName -PropertyType DWord -Value $valueData -Force
+
+Write-Output "Registry value '$valueName' set to $valueData under $regPath"
